50c7c621824ff8c6628c81e9f63ca5ca2af3aec6
Niels Boogaard <niels.boogaard@luminis.eu>
2020-09-01 09:06:20 +0200

    Updated dependencies because of vulnerabilities (2)

--- dist/filepond-plugin-media-preview.js
+++ dist/filepond-plugin-media-preview.js
@@ -1,5 +1,5 @@
 /*!
- * FilePondPluginMediaPreview 1.0.6
+ * FilePondPluginMediaPreview 1.0.7
  * Licensed under MIT, https://opensource.org/licenses/MIT/
  * Please visit undefined for details.
  */
