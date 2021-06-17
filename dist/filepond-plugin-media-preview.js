9e528b05ab9fea12c2d8eab05c9577f7e71b65b8
Niels Boogaard <niels.boogaard@luminis.eu>
2021-06-17 12:15:26 +0200

    Released version 1.0.9

--- dist/filepond-plugin-media-preview.js
+++ dist/filepond-plugin-media-preview.js
@@ -1,5 +1,5 @@
 /*!
- * FilePondPluginMediaPreview 1.0.8
+ * FilePondPluginMediaPreview 1.0.9
  * Licensed under MIT, https://opensource.org/licenses/MIT/
  * Please visit undefined for details.
  */
